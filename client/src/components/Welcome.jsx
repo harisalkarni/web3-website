8c5085f3571b11b683532220e8f07726f102d2ea
harisalkarni <haris.alkarni@gmail.com>
2022-08-16 19:03:44 +0700

    first commit

--- client/src/components/Welcome.jsx
+++ client/src/components/Welcome.jsx
@@ -4,7 +4,7 @@ import { SiEthereum} from 'react-icons/si'
 import { BsInfoCircle} from 'react-icons/bs'
 // import { TransactionContext} from ''
 // import { shortenAddress} from ''
-// import { Loader } from '.'
+import { Loader } from '.'
 
 const companyCommonStyles = 'min-h-[70px] sm:px-0 px-2 sm:min-w-[120px] flex justify-center items-center border-[0.5px] border-gray-400 text-sm font-light text-white'
 
@@ -13,10 +13,12 @@ const Input = ({ placeholder, name, type, value, handleChange}) => (
 )
 
 const Welcome = () => {
+
+
   return (
     <div className='flex w-full justify-center items-center'>
-        <div className='flex mf:flex-row flex-col items-start justify-between md:p-20 py-12 px-4'>
-            <div className='flex flex-1 justify-start items-start flex-col mf:mr-10'>
+        <div className='flex md:flex-row flex-col items-start justify-between md:p-20 py-12 px-4'>
+            <div className='flex flex-1 justify-start items-start flex-col md:mr-10'>
                 <h1 className='text-3xl sm:text-5xl text-white text-gradient py-1'>
                     Send Crypto <br />Across The World
                 </h1>
@@ -45,6 +47,39 @@ const Welcome = () => {
                     </div>
                     <div className={`rounded-br-2xl ${companyCommonStyles}`}>
                         Blockchain
+                    </div>
+                </div>
+            </div>
+
+            <div className='flex flex-col flex-1 items-center justify-start w-full md:mt-0 mt-10'>
+                <div className='p-3 justify-end items-start flex-col rounded-xl h-40 sm:w-72 w-full my-5 eth-card .white-glassmorphism'>
+                    <div className='flex justify-between flex-col w-full h-full'>
+                        <div className='flex justify-between items-start'>
+                            <div className='w-10 h-10 rounded-full border-2 border-white flex justify-center items-center'>
+                                <SiEthereum fontSize={21} color='#fff' />
+                            </div>
+                            <BsInfoCircle fontSize={17} color='#fff' />
+                        </div>
+                        <div>
+                            <p className='text-white font-light text-sm'>Address</p>
+                            <p className='text-white font-semibold text-lg mt-1'>Ethereum</p>
+                        </div>
+                    </div>
+                </div>
+                <div className='p-5 sm:w-96 w-full flex flex-col justify-start items-center blue-glassmorphism'>
+                    <Input placeholder='Address to' name="addressTo" type='text'  />
+                    <Input placeholder='Amount (ETH)' name="amount" type='number'  />
+                    <Input placeholder='Keyword (Gif)' name="keyword" type='text'  />
+                    <Input placeholder='Enter Message' name="message" type='text'  />
+
+                    <div className='h-[1px] w-full bg-gray-400 my-2'>
+                        {false ? (
+                            <Loader />
+                        ) : (
+                            <button type='button' className='text-white w-full mt-2 border-[1px] p-2 border-[#3d4f7c] hover:bg-[#3d4f7c] rounded-full cursor-pointer' >
+                            Send Now
+                            </button>
+                        )}
                     </div>
                 </div>
             </div>
