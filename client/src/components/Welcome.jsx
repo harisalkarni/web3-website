3c7aef8d50ee846675d093a29da261aa8a67c21b
harisalkarni <haris.alkarni@gmail.com>
2022-08-19 17:00:20 +0700

    first update

--- client/src/components/Welcome.jsx
+++ client/src/components/Welcome.jsx
@@ -2,9 +2,9 @@ import React, {useContext} from 'react'
 import { AiFillPlayCircle } from 'react-icons/ai'
 import { SiEthereum} from 'react-icons/si'
 import { BsInfoCircle} from 'react-icons/bs'
-// import { TransactionContext} from ''
-// import { shortenAddress} from ''
+
 import { Loader } from '.'
+import { TransactionContext } from '../context/TransactionContext'
 
 const companyCommonStyles = 'min-h-[70px] sm:px-0 px-2 sm:min-w-[120px] flex justify-center items-center border-[0.5px] border-gray-400 text-sm font-light text-white'
 
@@ -13,7 +13,7 @@ const Input = ({ placeholder, name, type, value, handleChange}) => (
 )
 
 const Welcome = () => {
-
+    // const {connectWallet} = useContext(TransactionContext);
 
   return (
     <div className='flex w-full justify-center items-center'>
@@ -25,7 +25,7 @@ const Welcome = () => {
                 <p className='text-left mt-5 text-white font-light md:w-9/12 w-11/12 text-base'>
                     Explore the Crypto world. Buy and sell cryptocurrencies easly on Krypto.
                 </p>
-                <button type='button' onClick='' className='flex flex-row justify-center items-center my-5 bg-[#2952e3] p-3 rounded-full cursor-pointer hover:bg-[#2546bd]' >
+                <button type='button' onClick={connectWallet} className='flex flex-row justify-center items-center my-5 bg-[#2952e3] p-3 rounded-full cursor-pointer hover:bg-[#2546bd]' >
                     <AiFillPlayCircle className='text-white mr-2' />
                     <p className='text-white text-base font-semibold'>Connect Wallet</p>
                 </button>
--- client/src/context/TransactionContext.jsx
+++ client/src/context/TransactionContext.jsx
@@ -8,4 +8,53 @@ const {ethereum} = window;
 const getEthereumContract = () => {
     const provider = new ethers.providers.Web3Provider(ethereum);
     const signer= provider.getSigner();
+    const transactionContract = new ethers.Contract(contractAddress, contractABI, signer)
+
+    console.log({
+        provider,
+        signer,
+        transactionContract
+    })
+}
+
+export const TransactionsProvider = ({children}) => {
+    
+    const [currentAccount, setCurrentAccount] = useState("");
+
+    const checkIfWalletIsConnected = async () => {
+        if(!ethereum) return alert("Please Install metamask")
+        const accounts = await ethereum.request({method: 'eth_accounts'})
+        console.log(accounts)
+    }
+
+
+    const connectWallet = async () => {
+        try {
+          if (!ethereum) return alert("Please install MetaMask.");
+    
+          const accounts = await ethereum.request({ method: "eth_requestAccounts", });
+    
+          setCurrentAccount(accounts[0]);
+          window.location.reload();
+        } catch (error) {
+          console.log(error);
+    
+          throw new Error("No ethereum object");
+        }
+      };
+
+    useEffect(() => {
+      checkIfWalletIsConnected();
+    
+      return () => {
+        second
+      }
+    }, [])
+    
+
+    return (
+        <TransactionContext.Provider value={{connectWallet}}>
+            {children}
+        </TransactionContext.Provider>
+    )
 }
