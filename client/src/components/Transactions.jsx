2667a7be6f10ca915032d071f3bca3f9e05ba1b8
harisalkarni <haris.alkarni@gmail.com>
2022-08-24 18:35:19 +0700

    4th update

--- client/src/components/Transactions.jsx
+++ client/src/components/Transactions.jsx
@@ -2,10 +2,15 @@ import React, {useContext} from 'react'
 import { TransactionContext } from '../context/TransactionContext'
 import dummyData from '../utils/dummyData'
 import { shortenAddress} from "../utils/shortenAddress"
+import useFetch from '../hooks/useFetch'
 
 
 
 const TransactionCard = ({ addressTo, addressFrom, timestamp, message, keyword, amount, url}) => {
+
+  const gifUrl = useFetch({keyword})
+
+
   return (
     <div className='bg-[#181918] m-4 flex flex-1
     2xl:min-w-[450px]
@@ -30,7 +35,7 @@ const TransactionCard = ({ addressTo, addressFrom, timestamp, message, keyword, 
             </>
           )}
         </div>
-        <img src="" alt="nature" className='w-full h-64 2xl:h-96 rounded-md shadow-lg object-cover' />
+        <img src={gifUrl || url} alt="gif" className='w-full h-64 2xl:h-96 rounded-md shadow-lg object-cover' />
         <div className='bg-black p-3 px-5 w-max rounded-3xl mt-5 shadow-2xl'>
           <p className='text-[#37c7da] font-bold'>{timestamp}</p>
         </div>
@@ -39,7 +44,7 @@ const TransactionCard = ({ addressTo, addressFrom, timestamp, message, keyword, 
   )
 }
 
-//update something lah
+
 
 const Transactions = () => { 
 
